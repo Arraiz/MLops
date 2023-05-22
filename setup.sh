df0668c3640331437e8f1cda2a1a02e93d77fb28
arraiz <mikeldiezgarcia@gmail.com>
2023-05-22 20:26:05 +0200

    fixed volumes

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ mkdir ${NEW_CONTAINER_NAME}
 # Set the new password
 NEW_PASSWORD="test2"
 
-custom_volume="./${NEW_CONTAINER_NAME}/data:/notebook_backup"
+custom_volume="./${NEW_CONTAINER_NAME}/data:/notebooks_backup"
 
 
 
@@ -17,13 +17,6 @@ custom_volume="./${NEW_CONTAINER_NAME}/data:/notebook_backup"
 sed -i   "s/container_name:.*/container_name: ${NEW_CONTAINER_NAME}/g" ${DOCKER_COMPOSE_FILE}
 # Search for the "volumes" line and replace it with the custom volume string
 sed -i '$d' "$DOCKER_COMPOSE_FILE"
-
 sed -i   "s|volumes:.*|volumes:\n      - $custom_volume|" "$DOCKER_COMPOSE_FILE"
 sed -i   "s|c.NotebookApp.password.*|c.NotebookApp.password = passwd('${NEW_PASSWORD}')|" jupyter_notebook_config.py
-#mv ${NEW_DOCKER_COMPOSE_FILE} ${DOCKER_COMPOSE_FILE}
-# Build and launch the container using Docker Compose
-##docker-compose up --build 
 
-# Restore the original Docker Compose file and Jupyter Notebook config
-#git checkout docker-compose.yml
-#git checkout jupyter_notebook_config.py
