e22b10291f4892241d68a36749f29ffdc574211e
arraiz <mikeldiezgarcia@gmail.com>
2023-05-22 13:42:54 +0200

    modified files

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -1,6 +1,6 @@
 from notebook.auth import passwd
 
-c.NotebookApp.password = passwd('new-password')
+c.NotebookApp.password = passwd('test')
 c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.port = 8888
 c.NotebookApp.open_browser = False
--- setup.sh
+++ setup.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-# Set the new container name
-NEW_CONTAINER_NAME="new-jupyter-container"
 
+DOCKER_COMPOSE_FILE="docker-compose.yml"
+# Set the new container name
+NEW_CONTAINER_NAME="test"
+mkdir ${NEW_CONTAINER_NAME}
 # Set the new password
-NEW_PASSWORD="new-password"
+NEW_PASSWORD="test"
+
+custom_volume="./${NEW_CONTAINER_NAME}/data:/notebook_backup"
+
+
 
 # Modify the container name in the Docker Compose file
-sed -i ''  "s/container_name: default/container_name: ${NEW_CONTAINER_NAME}/g" docker-compose.yml
+sed -i ''  "s/container_name:.*/container_name: ${NEW_CONTAINER_NAME}/g" ${DOCKER_COMPOSE_FILE}
+# Search for the "volumes" line and replace it with the custom volume string
+sed -i ''  "s|volumes:.*|volumes:\n      - $custom_volume|" "$DOCKER_COMPOSE_FILE"
 sed -i ''  "s|c.NotebookApp.password.*|c.NotebookApp.password = passwd('${NEW_PASSWORD}')|" jupyter_notebook_config.py
 
 # Build and launch the container using Docker Compose
-#docker-compose up --build
+##docker-compose up --build 
 
 # Restore the original Docker Compose file and Jupyter Notebook config
 #git checkout docker-compose.yml
